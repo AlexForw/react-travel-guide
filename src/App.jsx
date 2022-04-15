0c2b7a201d84fae4a504643ff1df878b2603c0a2
AlexForw <lmityushkin@mail.ru>
2022-04-15 11:55:55 +0300

    feat: add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { useEffect, useState } from 'react';
+import Header from './components/Header/Header';
 
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
 
-  const subtr = () => setCounter(counter - 1)
-  
-  const dupl = (arr) => [...arr, ...arr]
-console.log(dupl([1,2,3,4,5]))
   return (
-    <div className="App">
-      <h2>Counter: {counter}</h2>
-      <button onClick={subtr}>-</button>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-    </div>
+    <>
+    <Header />
+    </>
   );
 }
 
