1916a07ef59a9029fdd9eb3fcc45421c54570561
AlexForw <lmityushkin@mail.ru>
2022-04-15 14:50:07 +0300

    feat: add main header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
+import useStyles from './HeaderStyle'
+
 
 const Header = () => {
+    const styles = useStyles()
+
     return (
-        <div>
-            Hello
-        </div>
+        <AppBar className={styles.header}>
+            <Toolbar className={styles.toolbar}>
+                <Typography variant='h5' className={styles.typography}>Your own guide</Typography>
+                <Box display='flex'>
+                <Typography variant='h6' className={styles.typography}>Enter your place</Typography>
+
+                {/* </Autocomplete> */}
+                    <div className={styles.searchBox}>
+                        <div className={styles.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase placeholder='Search..' className={styles.inputInput}></InputBase>
+                    </div>
+                {/* <Autocomplete> */}
+
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 };
 
