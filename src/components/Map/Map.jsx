bf0faa43d89c97547e85d541cd699aae9f6d5ecd
AlexForw <lmityushkin@mail.ru>
2022-05-17 02:05:41 +0300

    feat: add Leaflet map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,21 @@
 import { Box } from '@mui/material';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+
+
 const Map = () => {
     return (
-        <Box flex={4} p={2} height='100vh' display='flex' gap={2}>
-            MAP 
+        <Box flex={4} p={2} height='90vh' display='flex' gap={2}>
+            <MapContainer style={{ height: '100%', width: '100%' }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        The cool place!
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </Box>
     );
 };
