bd789bc93927edf25637d3af47d9dfc0c3c230ef
AlexForw <lmityushkin@mail.ru>
2022-05-17 13:57:32 +0300

    feat: add select menu

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,10 +1,31 @@
-import { Box } from '@mui/material';
+import { Box, Typography, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 
 const List = () => {
+
+
     return (
-        <Box flex={1} p={2}>
-            LIST
+        <Box flex={2} p={2}>
+            <Typography variant='h4'>Places around you</Typography>
+
+            <FormControl>
+                <InputLabel>Type</InputLabel>
+                <Select value={type}>
+                    <MenuItem value='restaurants'>Restaurants</MenuItem>
+                    <MenuItem value='hotel'>Hotel</MenuItem>
+                    <MenuItem value='attractions'>Attractions</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3.0</MenuItem>
+                    <MenuItem value={4}>Above 4.0</MenuItem>
+                    <MenuItem value={4.5}>Above 4.5</MenuItem>
+                </Select>
+            </FormControl>
         </Box>
     );
 };
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,7 +5,7 @@ import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 const Map = () => {
     return (
         <Box flex={4} p={2} height='90vh' display='flex' gap={2}>
-            <MapContainer style={{ height: '100%', width: '100%' }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer style={{ height: '100%', width: '100%', margin:'50 50 50 50' }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
