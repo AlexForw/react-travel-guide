7c836ee023563721c2a9044faa36b9cbe4e9559a
AlexForw <lmityushkin@mail.ru>
2022-05-18 23:20:54 +0300

    feat: add some thoughts about getting bounds

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,26 @@ import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 import './App.css'
+import { getPlacesData } from './api/index'
+import { useEffect, useState } from 'react';
 
 
 const App = () => {
+  const [places, setPlaces] = useState('')
 
+  const [coordinates, setCoordinates] = useState({})
+  const [bounds, setBounds] = useState(null)
+  console.log(coordinates);
+
+  useEffect(()=>{
+    getPlacesData().then(data => setPlaces(data))
+  },[])
   return (
     <>
       <Header />
       <Box display='flex'>
         <List />
-        <Map />
+        <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates}/>
       </Box>
     </>
   );
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,22 @@
 import { Box } from '@mui/material';
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import L from 'leaflet'
+import { useEffect, useRef } from 'react'
 
 
-const Map = () => {
+const Map = ({ setBounds, setCoordinates, coordinates }) => {
+    let refElem = useRef('')
+
+    useEffect(() => {
+        console.log(refElem.current)
+    }, [refElem])
+    
+    
+    console.log(L.FitBoundsOption);
+
     return (
         <Box flex={4} p={2} height='90vh' display='flex' gap={2}>
-            <MapContainer style={{ height: '100%', width: '100%', margin:'50 50 50 50' }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer ref={(e)=> refElem} style={{ height: '100%', width: '100%', margin: '50 50 50 50' }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
