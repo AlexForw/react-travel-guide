28915fb9c2d75b49499a1fde240eb5dcb72899b9
AlexForw <lmityushkin@mail.ru>
2022-05-19 21:49:47 +0300

    feat: add main appearance

--- src/assets/CardElem/CardElem.jsx
+++ src/assets/CardElem/CardElem.jsx
@@ -3,7 +3,7 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { Divider } from '@mui/material';
+import { Box, Divider } from '@mui/material';
 
 
 const CardElem = ({ data }) => {
@@ -14,11 +14,19 @@ const CardElem = ({ data }) => {
                 component="img"
                 height="30%"
                 width="100"
-                image={data.photo?.images?.medium?.url}
-                alt="City"
+                image={data.photo ? data.photo?.images?.medium?.url : 'https://api.interior.ru/media/images/DESIGN/Modnoe%20Mesto/Russki_restaurant/cover_RUSKI_interior_5.jpg'}
+                alt={data.name}
             />
             <CardContent>
                 <Typography variant='h6'>{data.name}</Typography>
+                <Box sx={{display:'flex', justifyContent:'space-between'}}>
+                    <Typography variant='subtitle1'>Price</Typography>
+                    <Typography variant='subtitle1'>{data.price ? data.price : '$5 - $10'}</Typography>
+                </Box>
+                <Box sx={{display:'flex', justifyContent:'space-between'}}>
+                    <Typography variant='subtitle1'>Ranking</Typography>
+                    <Typography variant='subtitle1'>{data.ranking ? data.ranking : 'without rank'}</Typography>
+                </Box>
                 <Divider sx={{ height: 5, m: 0.5 }} orientation="horizontal" />
                 <Typography variant="body2" color="text.secondary">
 
