cc789125f230857898ad0f06df06846ae8e97b73
AlexForw <lmityushkin@mail.ru>
2022-05-19 14:43:35 +0300

    feat: add obj values in states

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -15,7 +15,7 @@ const Map = ({ setBounds, bounds, setCoordinates, coordinates }) => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <UseMapElem setBounds={setBounds}/>
+                <UseMapElem setBounds={setBounds} setCoordinates={setCoordinates}/>
                 <Marker position={[51.505, -0.09]}>
                     <Popup>
                         The cool place!
