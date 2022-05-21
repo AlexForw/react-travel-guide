e79fb49d4840fc16d6064c50f5b0c226f91a639d
AlexForw <lmityushkin@mail.ru>
2022-05-21 19:56:26 +0300

    feat: add getCities asyncThunk

--- src/store/cordSlice.js
+++ src/store/cordSlice.js
@@ -2,9 +2,34 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 
+export const getCities = createAsyncThunk(
+    'cord/getCities',
+    async function (city, { rejectWithValue }) {
+        try {
+
+            const URL = 'https://spott.p.rapidapi.com/places/autocomplete'
+
+
+            const { data: { data } } = await axios.get(URL, {
+
+                params: { limit: '10', skip: '0', q: { city }, type: 'CITY' },
+                headers: {
+                    'X-RapidAPI-Host': 'spott.p.rapidapi.com',
+                    'X-RapidAPI-Key': 'ef2b898bd6msh61efe35f6e31be6p175661jsn769f45957103'
+                }
+            })
+
+            return data
+        } catch (error) {
+            return rejectWithValue(error.message)
+        }
+    }
+)
+
+
 export const getPlacesData = createAsyncThunk(
     'cord/getApi',
-    async function (info,{rejectWithValue}) {
+    async function (info, { rejectWithValue }) {
         try {
 
             const URL = `https://travel-advisor.p.rapidapi.com/${info.type}/list-in-boundary`
@@ -29,12 +54,14 @@ export const getPlacesData = createAsyncThunk(
     }
 )
 
+
 const initialState = {
     coordinates: {},
     bounds: {},
     places: [],
     status: null,
     error: null,
+    cities: [],
 }
 
 export const cordSlice = createSlice({
@@ -55,12 +82,15 @@ export const cordSlice = createSlice({
         },
         [getPlacesData.fulfilled]: (state, action) => {
             state.status = 'resolved'
-            state.places = action.payload.filter((e)=> e.name && e.num_reviews)
+            state.places = action.payload.filter((e) => e.name && e.num_reviews)
             state.error = null
         },
         [getPlacesData.rejected]: (state, action) => {
             state.status = 'rejected'
             state.error = action.payload
+        },
+        [getCities.fulfilled]: (state, action) => {
+            state.cities = action.payload.filter((e) => e.name && e.num_reviews)
         },
     }
 })
